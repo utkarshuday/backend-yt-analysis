cec6d673ce5960c85ffad5be2bfa28418ca1b088
utkarshuday <120191592+utkarshuday@users.noreply.github.com>
2024-07-24 02:12:30 +0530

    more changes

--- app/sentiments.py
+++ app/sentiments.py
@@ -34,17 +34,16 @@ DetectorFactory.seed = 0
 #   comments = df[0].tolist()
 #   return comments
 
+# comments = ['16:50 is the best \n\n\n1:12:30 is where I started understanding how time Flies', '42:51', '6:13  Mujra 😂', 'Thanks Kullu bhai for making more relatable 😂', 'This recap far better than  2023 😂😂😂', 'Watching it today,2024 but thanks man everyone ..Taarak Mehta chalta tha khana khate time,abb ye reaction videos chalta hai..Great Job guys.', '1:57 bro needs coffee', 'Present from 2024 🎉',
+#             '01:07:38 kisi ka rick kisi ka roll kullu 😂😂😂', '17:39 if you wanna find a great meme so see this one legend 😅', '1:07:00 bhai ye vali full video ho kisi k pas to link bhejo😂 spiderman wali kya Kamal ki meme he usme😜', "Don't know why 30:20 he looks like Biswa.", 'Annu kanppor is the biggest f#ck up of bollywood and tv industry togather \nHutiya \nYe to gali ke nukkad par paan tapri bhi deserve nahi karta hai', '1:07:40 😂', '59:00😂', '59:00😂', 'Very disappointed']
+
 
 def replace_emojis(text):
   return demoji.replace_with_desc(text, sep=" ")
 
 
-def remove_colons_and_numbers(text):
-  pattern = r'[:\d]'
-  return re.sub(pattern, '', text)
-
-
 def is_english(text):
+  text = replace_emojis(text)
   try:
     if detect(text) == 'en':
       return text
@@ -54,20 +53,11 @@ def is_english(text):
 
 
 def clean_comments(comments):
-  # Convert the list of comments to a pandas Series
   comments_series = pd.Series(comments).drop_duplicates()
-
-  # Apply the cleaning functions using vectorized operations
-  comments_series = comments_series.apply(replace_emojis)
-  comments_series = comments_series.apply(remove_colons_and_numbers)
+  comments_series = comments_series.str.replace(r'[:\d]', '', regex=True)
   comments_series = comments_series.apply(is_english)
-
-  # Drop any NaN values
   comments_series = comments_series.dropna()
-
-  # Convert the cleaned Series back to a list
   cleaned_comments = comments_series.tolist()
-
   return cleaned_comments
 
 
@@ -117,3 +107,6 @@ def getSentimentScores(comments, videoId):
                 sentimentCounts=sentiment_counts
                 )
   return result
+
+
+# print(clean_comments(comments))
