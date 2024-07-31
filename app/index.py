919f57eae85a16818477cf4407b03c7069dde7d9
utkarshuday <120191592+utkarshuday@users.noreply.github.com>
2024-07-31 17:17:36 +0530

    worker-feature

--- app/index.py
+++ app/index.py
@@ -1,7 +1,18 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from app.yt import getChannels, getChannelDetails, getRawComments
-from app.sentiments import getSentimentScores
+from celery import Celery
+import celery.states as states
+import os
+
+host = os.environ['UPSTASH_REDIS_HOST']
+password = os.environ['UPSTASH_REDIS_PASSWORD']
+port = os.environ['UPSTASH_REDIS_PORT']
+connection_link = "rediss://:{}@{}:{}?ssl_cert_reqs=CERT_REQUIRED".format(
+    password, host, port)
+
+celery = Celery('tasks', backend='redis://localhost:6379',
+                broker='redis://localhost:6379', result_expires=60 * 60 * 16)
 
 app = Flask(__name__)
 CORS(app)
@@ -31,6 +42,15 @@ def get_sentiment_analysis(videoId):
   if isinstance(comments, dict) and comments.get('error'):
     return jsonify(comments.get('message', {'message': 'Error occurred'}))
   print(f'Collected comments from API for videoId {videoId} ...')
-  result = getSentimentScores(comments, videoId)
+  task = celery.send_task('tasks.sentiment_scores', args=[comments, videoId])
   print(f'Sending response for videoId {videoId} ...')
-  return jsonify(result)
+  return jsonify({'id': task.id})
+
+
+@app.route('/check/<string:task_id>')
+def get_result(task_id):
+  res = celery.AsyncResult(task_id)
+  if res.state == states.PENDING:
+    return res.state
+  else:
+    return jsonify(res.result)
--- app/sentiments.py
+++ app/sentiments.py
@@ -3,39 +3,8 @@ from textblob import TextBlob
 import pandas as pd
 import numpy as np
 import demoji
-import re
 
 DetectorFactory.seed = 0
-
-
-# def replace_emojis(text):
-#   return demoji.replace_with_desc(text, sep=" ")
-
-
-# def remove_colons_and_numbers(text):
-#   pattern = r'[:\d]'
-#   return re.sub(pattern, '', text)
-
-
-# def is_english(text):
-#   try:
-#     if detect(text) == 'en':
-#       return text
-#     return np.nan
-#   except:
-#     return np.nan
-
-
-# def clean_comments(comments):
-#   df = pd.DataFrame(comments).drop_duplicates()
-#   df[0] = df[0].apply(replace_emojis).apply(
-#       remove_colons_and_numbers).apply(is_english)
-#   df = df.dropna()
-#   comments = df[0].tolist()
-#   return comments
-
-# comments = ['16:50 is the best \n\n\n1:12:30 is where I started understanding how time Flies', '42:51', '6:13  Mujra 😂', 'Thanks Kullu bhai for making more relatable 😂', 'This recap far better than  2023 😂😂😂', 'Watching it today,2024 but thanks man everyone ..Taarak Mehta chalta tha khana khate time,abb ye reaction videos chalta hai..Great Job guys.', '1:57 bro needs coffee', 'Present from 2024 🎉',
-#             '01:07:38 kisi ka rick kisi ka roll kullu 😂😂😂', '17:39 if you wanna find a great meme so see this one legend 😅', '1:07:00 bhai ye vali full video ho kisi k pas to link bhejo😂 spiderman wali kya Kamal ki meme he usme😜', "Don't know why 30:20 he looks like Biswa.", 'Annu kanppor is the biggest f#ck up of bollywood and tv industry togather \nHutiya \nYe to gali ke nukkad par paan tapri bhi deserve nahi karta hai', '1:07:40 😂', '59:00😂', '59:00😂', 'Very disappointed']
 
 
 def replace_emojis(text):
@@ -43,7 +12,6 @@ def replace_emojis(text):
 
 
 def is_english(text):
-  text = replace_emojis(text)
   try:
     if detect(text) == 'en':
       return text
@@ -55,6 +23,7 @@ def is_english(text):
 def clean_comments(comments):
   comments_series = pd.Series(comments).drop_duplicates()
   comments_series = comments_series.str.replace(r'[:\d]', '', regex=True)
+  comments_series = comments_series.apply(replace_emojis)
   comments_series = comments_series.apply(is_english)
   comments_series = comments_series.dropna()
   cleaned_comments = comments_series.tolist()
