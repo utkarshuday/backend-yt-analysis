c576a3cefa7088239537e93300d3a582318fbdff
utkarshuday <120191592+utkarshuday@users.noreply.github.com>
2024-07-31 19:22:34 +0530

    checking

--- app/tasks.py
+++ app/tasks.py
@@ -4,17 +4,22 @@ from app.sentiments import getSentimentScores
 import redis
 import json
 
-host = os.environ['UPSTASH_REDIS_HOST']
-password = os.environ['UPSTASH_REDIS_PASSWORD']
-port = os.environ['UPSTASH_REDIS_PORT']
-connection_link = "rediss://:{}@{}:{}?ssl_cert_reqs=CERT_REQUIRED".format(
-    password, host, port)
+# host = os.environ['UPSTASH_REDIS_HOST']
+# password = os.environ['UPSTASH_REDIS_PASSWORD']
+# port = os.environ['UPSTASH_REDIS_PORT']
+# connection_link = "rediss://:{}@{}:{}?ssl_cert_reqs=CERT_REQUIRED".format(
+#     password, host, port)
 
 
-celery = Celery('tasks', backend=connection_link,
-                broker=connection_link, result_expires=60 * 60 * 16)
+# celery = Celery('tasks', backend=connection_link,
+#                 broker=connection_link, result_expires=60 * 60 * 16)
+# redis_client = redis.StrictRedis(
+#     host=host, port=port, password=password, ssl=True, decode_responses=True)
+
+celery = Celery('tasks', backend='redis://localhost:6379',
+                broker='redis://localhost:6379', result_expires=60 * 60 * 16)
 redis_client = redis.StrictRedis(
-    host=host, port=port, password=password, ssl=True, decode_responses=True)
+    host='localhost', port=6379, db=0, decode_responses=True)
 
 
 @celery.task
